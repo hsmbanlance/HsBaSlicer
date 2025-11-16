85b5872c753927402cee972049f4259e7e8d97e7
hsmbanlance <hsmbalance@outlook.com>
2025-11-16 18:40:26 +0800

    添加对FullTopoModel的切片

--- LibHsBaSlicer/Slice/mesh_slice.hpp
+++ LibHsBaSlicer/Slice/mesh_slice.hpp
@@ -11,6 +11,9 @@ namespace HsBa::Slicer
 	//不安全的切片，包含不封闭轮廓。如果需要封闭的轮廓，请使用Slice。
 	//在送丝的工艺下可以考虑使用不安全切片，使用SLA等面成型工艺时不考虑使用
 	UnSafePolygons UnSafeSlice(const IModel& model, const float height);
+
+	Polygons SliceLua(const IModel& model, const std::string& script, const float height);
+	UnSafePolygons UnSafeSliceLua(const IModel& model, const std::string& script, const float height);
 }// namespace HsBa::Slicer
 
 #endif // !HSBA_SLICER_MESH_SLICE_HPP
